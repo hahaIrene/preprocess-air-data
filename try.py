998001e8eda9be4d8c1a0c7393be6410d4f19cff
unknown <you@example.com>
2023-07-25 22:10:23 +0800

    edward

--- try.py
+++ try.py
@@ -2,30 +2,30 @@ import pandas as pd
 import os
 
 
-# def dataMerge(data_path, output_file, project):
-#     project = []
-#     # project_1032 = []
+def dataMerge(data_path, output_file, project):
+    project = []
+    # project_1032 = []
 
-#     for fileName in os.listdir(data_path):
-#         fileSplit = fileName.split('_')
-#         if fileSplit[0] == project:
-#             project.append(fileName)
+    for fileName in os.listdir(data_path):
+        fileSplit = fileName.split('_')
+        if fileSplit[0] == project:
+            project.append(fileName)
 
-#     data = []
-#     for fileName in project:
-#         subPath = os.path.join(data_path, fileName)
-#         data = pd.read_csv(subPath)
-#         data.append(data)
-#     mergeData = pd.concat(data, axis=0)
-#     mergeData.to_csv(output_file)
+    data = []
+    for fileName in project:
+        subPath = os.path.join(data_path, fileName)
+        data = pd.read_csv(subPath)
+        data.append(data)
+    mergeData = pd.concat(data, axis=0)
+    mergeData.to_csv(output_file)
 
-#     # data_1032 = []
-#     # for fileName in project_1032:
-#     #     subPath = os.path.join(data_path, fileName)
-#     #     data = pd.read_csv(subPath)
-#     #     data_1032.append(data)
-#     # mergeData_1032 = pd.concat(data_1032, axis=0)
-#     # mergeData_1032.to_csv(output_file_1032)
+    # data_1032 = []
+    # for fileName in project_1032:
+    #     subPath = os.path.join(data_path, fileName)
+    #     data = pd.read_csv(subPath)
+    #     data_1032.append(data)
+    # mergeData_1032 = pd.concat(data_1032, axis=0)
+    # mergeData_1032.to_csv(output_file_1032)
 
 # if __name__ == "__main__":
 #     months = ["01", "04", "07"]
@@ -38,16 +38,17 @@ import os
 #         dataMerge(data_path, output_file_756, "1032")
 
 
-#groupby
 
+# groupby
 dataMerges = pd.read_csv( r"./output/01_756_merge.csv")
-print(dataMerges)
+# print(dataMerges)
 
-# 建字典
+# 建字典 結構從
 unique_device_ids =list(dataMerges['DEVICE_ID'].unique())
-# print('7494382694' in unique_device_ids)
 
-# print('7522863555' in unique_device_ids)
+#驗證是否拿到id
+# print(7494382694 in unique_device_ids)
+# print(7522863555 in unique_device_ids)
 dataDict = {
     
 }
@@ -89,8 +90,14 @@ for index, row in dataMerges.iterrows():
 # # # groupBy = dataMerges.groupby(['DEVICE_ID'])
 # # # print(groupBy)
 
-# 轉成dataframe(array)
-#  
+# dataframe(array)
+# 然後aggregate
+# 
 
 
-aggre
+for key in dataDict:
+    # dataDict[key]
+
+    df = pd.DataFrame(dataDict[key], columns=['DEVICE_ID','LON', 'LAT', 'TIME', 'PM2_5(μg/m3)','PM10(mg/m3)',
+                                                      'TEMPERATURE(℃)','HUMIDITY(%)','WIND_SPEED(m/sec)','VOC(ppb)','WIND_DIRECT(degrees)'])
+    df.to_csv(f"{key}_01_756.csv")
